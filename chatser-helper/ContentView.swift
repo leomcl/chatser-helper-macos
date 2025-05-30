13cae3907447e97d8547ef14fe645a75f5f0c28d
Leo Mclaughlin <leo.mcl616@gmail.com>
2025-05-30 20:12:03 +0100

    first

---
+++ chatser-helper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chatser-helper
+//
+//  Created by Leo Mclaughlin on 30/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chatser-helper/chatser_helperApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatser_helperApp.swift
+//  chatser-helper
+//
+//  Created by Leo Mclaughlin on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct chatser_helperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
